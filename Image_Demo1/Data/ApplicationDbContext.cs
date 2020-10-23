394dbb5c23dbd67443f48e00b990244bd399cfa3
Lethikimvui <55648393+Lethikimvui@users.noreply.github.com>
2020-10-23 21:39:11 +0700

    Add project files.

---
+++ Image_Demo1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Image_Demo1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcCoreUploadAndDisplayImage_Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
